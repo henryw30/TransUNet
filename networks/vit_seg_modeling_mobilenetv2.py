0e9cbcdab42e0877569f170112e71ba39459c077
Henry <henryw2@illinois.edu>
2021-05-19 17:10:06 -0400

    more pruning of mobilenet

--- networks/vit_seg_modeling_mobilenetv2.py
+++ networks/vit_seg_modeling_mobilenetv2.py
@@ -132,7 +132,7 @@ class MobileNetV2(nn.Module):
         x = self.features(x)
         x = self.conv(x)
         x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
+        #x = x.view(x.size(0), -1)
         #x = self.classifier(x)
         return x
 
