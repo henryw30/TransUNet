6648594a10c802ad1c8f993c61fd65a1b5e29572
Henry <henryw2@illinois.edu>
2021-05-20 01:09:24 -0400

    sdlfkj

--- networks/vit_seg_modeling_mobilenetv2.py
+++ networks/vit_seg_modeling_mobilenetv2.py
@@ -136,9 +136,9 @@ class MobileNetV2(nn.Module):
         for i in range(len(self.features)):
             x = self.features[i](x)
             if i in stage_num:
-                temp = nn.functional.avg_pool2d(x, 2, stride=2)
-                print(temp.shape)
-                skip_feeder.append(temp)
+                #temp = nn.functional.avg_pool2d(x, 2, stride=2)
+                #print(temp.shape)
+                skip_feeder.append(x)
 
         #x = self.features(x)
         x = self.conv(x)
