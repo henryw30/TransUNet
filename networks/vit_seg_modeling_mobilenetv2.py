76f915dcbba99054db0307ad143be4af08d5ce6f
Henry <henryw2@illinois.edu>
2021-05-19 23:45:00 -0400

    transpose a

--- networks/vit_seg_modeling_mobilenetv2.py
+++ networks/vit_seg_modeling_mobilenetv2.py
@@ -142,7 +142,7 @@ class MobileNetV2(nn.Module):
         temp = x.flatten(2)
         temp = temp.transpose(-1, -2)
         print("1.3: ", temp.shape)
-        temp = nn.functional.avg_pool1d(temp, 512)
+        temp = nn.functional.avg_pool1d(temp, 512, stride=3)
         temp = temp.transpose(-1, -2)
         temp = temp.unflatten(2, (8, 8))
         print("1.4: ", temp.shape)
